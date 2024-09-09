e176e3d4ed7b1e443a80d3c00f93dd97e11e1608
Pamela Alcantara <pame_23a@hotmail.com>
2024-09-09 17:28:03 -0500

    columna modificada

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_09_09_210341) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
-  create_table "offers", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "requirements"
-    t.date "start_date"
-    t.date "end_date"
-    t.integer "availability"
-    t.bigint "user_id", null: false
-    t.string "address"
-    t.float "latitude"
-    t.float "logitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_offers_on_user_id"
-  end
-  
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +42,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_09_210341) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "offers", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "requirements"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "availability"
+    t.bigint "user_id", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "logitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,10 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_09_210341) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "offers", "users"
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
+  add_foreign_key "offers", "users"
 end
