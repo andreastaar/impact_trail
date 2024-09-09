8a8c287f76f81d927b27ccd655bcabf03a8c4d10
Milagros Avila <mavilam.8@gmail.com>
2024-09-09 17:18:28 -0500

    creacion de tablas application y reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_09_210341) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_09_09_221516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
-  create_table "offers", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "requirements"
-    t.date "start_date"
-    t.date "end_date"
-    t.integer "availability"
-    t.bigint "user_id", null: false
-    t.string "address"
-    t.float "latitude"
-    t.float "logitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_offers_on_user_id"
-  end
-  
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +42,43 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_09_210341) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "applications", force: :cascade do |t|
+    t.string "status"
+    t.bigint "offer_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_applications_on_offer_id"
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "requirements"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "availability"
+    t.bigint "user_id", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "logitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.float "rating"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "offer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_reviews_on_offer_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,10 +99,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_09_210341) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "offers", "users"
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
+  add_foreign_key "applications", "offers"
+  add_foreign_key "applications", "users"
+  add_foreign_key "offers", "users"
+  add_foreign_key "reviews", "offers"
+  add_foreign_key "reviews", "users"
 end
