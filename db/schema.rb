867ea68210427f9d36128981bd229a68fae1b01b
Pamela Alcantara <pame_23a@hotmail.com>
2024-09-10 11:54:46 -0500

    estilo role

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_09_210341) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_09_09_221516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,9 +101,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_09_210341) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
-  add_foreign_key "offers", "users"
-
   add_foreign_key "applications", "offers"
   add_foreign_key "applications", "users"
   add_foreign_key "offers", "users"
